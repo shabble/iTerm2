4f758318ba20e6d56a9c182f49fa91bb23608fe8
Tom Feist <shabble@metavore.org>
2011-06-09 07:24:55 +0100

    forgot to add a bunch of prefs helpers

---
+++ Prefs/PreferencesAppearanceHelper.h
@@ -0,0 +1,16 @@
+//
+//  PreferencesAppearanceHelper.h
+//  iTerm
+//
+//  Created by shabble on 09/06/2011.
+//  Copyright 2011 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface PreferencesAppearanceHelper : NSObject {
+
+}
+
+@end
---
+++ Prefs/PreferencesGlobalKeybindingsHelper.h
@@ -0,0 +1,40 @@
+//
+//  PreferencesGlobalKeybindingsHelper.h
+//  iTerm
+//
+//  Created by shabble on 09/06/2011.
+//  Copyright 2011 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface PreferencesGlobalKeybindingsHelper : NSObject {
+    
+    // Keyboard ------------------------------
+    int defaultControl;
+    IBOutlet NSPopUpButton* controlButton;
+    int defaultLeftOption;
+    IBOutlet NSPopUpButton* leftOptionButton;
+    int defaultRightOption;
+    IBOutlet NSPopUpButton* rightOptionButton;
+    int defaultLeftCommand;
+    IBOutlet NSPopUpButton* leftCommandButton;
+    int defaultRightCommand;
+    IBOutlet NSPopUpButton* rightCommandButton;
+    
+    int defaultSwitchTabModifier;
+    IBOutlet NSPopUpButton* switchTabModifierButton;
+    int defaultSwitchWindowModifier;
+    IBOutlet NSPopUpButton* switchWindowModifierButton;
+    
+    IBOutlet NSButton* deleteSendsCtrlHButton;
+    
+    IBOutlet NSTableView* globalKeyMappings;
+    IBOutlet NSTableColumn* globalKeyCombinationColumn;
+    IBOutlet NSTableColumn* globalActionColumn;
+    IBOutlet NSButton* globalRemoveMappingButton;
+    IBOutlet NSButton* globalAddNewMapping;
+}
+
+@end
