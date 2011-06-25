e6d492278eb16222718c8401b8820383f9a5af80
Tom Feist <shabble@metavore.org>
2011-06-25 01:07:57 +0100

    added stubs of EventTapManager, pending its possible removal from app/

---
+++ Misc/EventTapManager.h
@@ -0,0 +1,16 @@
+//
+//  EventTapManager.h
+//  iTerm
+//
+//  Created by shabble on 24/06/2011.
+//  Copyright 2011 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface EventTapManager : NSObject {
+
+}
+
+@end
---
+++ Misc/EventTapManager.m
@@ -0,0 +1,14 @@
+//
+//  EventTapManager.m
+//  iTerm
+//
+//  Created by shabble on 24/06/2011.
+//  Copyright 2011 . All rights reserved.
+//
+
+#import "EventTapManager.h"
+
+
+@implementation EventTapManager
+
+@end
